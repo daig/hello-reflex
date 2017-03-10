6c34ce4a58a17bfe5f3a39d85836a87c7716eb02
傣जी <daig@sodality.cc>
2017-03-09 19:02:21 -0500

    use jsaddle warp on ghc

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Reflex.Dom
+import Reflex.Dom.Core
+import Language.Javascript.JSaddle.Warp (run)
+import Control.Monad (forever)
+import Control.Concurrent (threadDelay)
+import Control.Monad.IO.Class (liftIO)
 
-main = mainWidget $ el "div" $ text "Hello, Reflex!"
+main :: IO ()
+main = run 8000 (mainWidget app >> dontClose)
+  where
+    {-This is needed on ghc to prevent the main thread from terminating
+     - after loading the page, and killing the warp websocket -}
+    dontClose = forever (liftIO (threadDelay 1000000000))
+
+
+app ::  MonadWidget t m => m ()
+app = el "div" $ text "Hello, Reflex!"
